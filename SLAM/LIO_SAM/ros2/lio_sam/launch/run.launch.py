742d9d40edd7fc7cde9222c0bd0e1eb2520924a6
timofeenko-mf <timofeenko@ms-17.ru>
2026-05-01 22:52:39 +0300

    FIX: LIO not LIWO

--- SLAM/LIO_SAM/ros2/lio_sam/launch/run.launch.py
+++ SLAM/LIO_SAM/ros2/lio_sam/launch/run.launch.py
@@ -52,20 +52,20 @@ def generate_launch_description():
         #         'robot_description': Command(['xacro', ' ', xacro_path])
         #     }]
         # ),
-        # Node(
-        #     package='lio_sam',
-        #     executable='lio_sam_imuPreintegration',
-        #     name='lio_sam_imuPreintegration',
-        #     parameters=[parameter_file],
-        #     output='screen'
-        # ),
         Node(
             package='lio_sam',
-            executable='lio_sam_wheelInertialPreintegration',
-            name='lio_sam_WI_Preintegration',
+            executable='lio_sam_imuPreintegration',
+            name='lio_sam_imuPreintegration',
             parameters=[parameter_file],
-            output='screen',
+            output='screen'
         ),
+        # Node(
+        #     package='lio_sam',
+        #     executable='lio_sam_wheelInertialPreintegration',
+        #     name='lio_sam_WI_Preintegration',
+        #     parameters=[parameter_file],
+        #     output='screen',
+        # ),
         Node(
             package='lio_sam',
             executable='lio_sam_imageProjection',
